84e78c3df09968c18937b85fff7ece9f46d0be0d
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-11-17 19:30:03 -0700

    begin structure of forEach loop to create alphabet

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 const alphabet = Array(26)
   .fill(null)
   .map((_, i) => i)
-console.log(alphabet)
+
+function alphabetCreator() {
+  alphabet.forEach((element) => console.log(element))
+}
 
 export function App() {
   return (
@@ -11,6 +14,7 @@ export function App() {
       <h1>Do you want to build a snowman?</h1>
       <main className="d-flex">
         <img src="/snowman-images/step_7.png" height="300px"></img>
+        <section>{alphabetCreator()}</section>
       </main>
     </div>
   )

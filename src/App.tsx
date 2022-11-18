7a054d5e7197b72400ccfbf3c6f2722f26f577fc
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-11-17 19:07:00 -0700

    adds basic CSS & title

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 import React from 'react'
 
+const alphabetButtons: [] = [26]
+
+Array(25)
+  .fill(null)
+  .map((_, i) => i)
+console.log(Array)
+
 export function App() {
-  return <div>Hello, World </div>
+  return (
+    <div>
+      <h1>Do you want to build a snowman?</h1>
+      <main></main>
+    </div>
+  )
 }

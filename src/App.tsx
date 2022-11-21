15a4b9ee3c51b54dabc501e74a134c710109ee4b
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-11-21 09:41:43 -0700

    implements a function to render 26 buttons!

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,23 @@ const alphabet = Array(26)
   .fill(null)
   .map((_, i) => i)
 
-function alphabetCreator() {
-  alphabet.forEach((element) => console.log(element))
+function AlphabetCreator() {
+  const newAlphabet = alphabet.map((letter) => (
+    <li key={letter}>
+      <button key={letter}>{letter}</button>
+    </li>
+  ))
+  return <ul>{newAlphabet}</ul>
 }
 
 export function App() {
   return (
-    <div>
+    <>
       <h1>Do you want to build a snowman?</h1>
       <main className="d-flex">
         <img src="/snowman-images/step_7.png" height="300px"></img>
-        <section>{alphabetCreator()}</section>
+        <AlphabetCreator />
       </main>
-    </div>
+    </>
   )
 }

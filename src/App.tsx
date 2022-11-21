33b332e4b65a1df63ca826f23a9829493bb9aa15
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-11-21 15:15:35 -0700

    logs a random word to the console

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import wordsArray from './words.json'
 
-function AlphabetCreator() {
+function RandomWordGenerator(): string {
+  const wordsList = wordsArray
+  const randomWord = wordsList[Math.floor(Math.random() * wordsList.length)]
+  return randomWord
+}
+console.log(RandomWordGenerator())
+
+function AlphabetGenerator() {
   const alphaArray = Array.from(Array(26)).map((e, i) => i + 65)
   const alphabet = alphaArray.map((x) => String.fromCharCode(x))
   const liAlphabet = alphabet.map((letter) => (
@@ -17,7 +25,13 @@ export function App() {
       <h1>Do you want to build a snowman?</h1>
       <main className="d-flex">
         <img src="/snowman-images/step_7.png" height="300px"></img>
-        <AlphabetCreator />
+        <button onClick={() => RandomWordGenerator()}>
+          Press here for a random word!
+        </button>
+        <ul>
+          <li>{} </li>
+        </ul>
+        <AlphabetGenerator />
       </main>
     </>
   )

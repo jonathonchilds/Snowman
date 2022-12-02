953efc7ba0f4f98d79a8f7f8ae2c8df5a1f9f63f
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-12-01 19:08:33 -0700

    cleaned up comments

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,7 @@ const randomWord =
   wordsArray[Math.floor(Math.random() * wordsArray.length)].toUpperCase()
 
 export function App() {
-  // updated the state with letters missing and re-render
-
   const [guesses, setGuesses] = useState<string[]>([])
-
-  // function compareClickWithRandomWord() {
-  //   //  Compare each button press to if that value is contained within .randomWord: string
-  //   //
-  //   //  If it is, identify where (and grab that letter's index from .randomWord)
-  //   //
-  //   //  Method to return the index number of where a button matches a letter within .randomWord
-  //   //
-  //   //  Display the letter (utilizing the corresponding index) on the game board
-  // }
 
   function AlphabetButtons() {
     const liAlphabet = Array.from(Array(26))
@@ -32,52 +20,50 @@ export function App() {
       ))
     return <ul>{liAlphabet}</ul>
 
-    //const guessedWordArray = []
-
     function assignKey(event: React.MouseEvent<HTMLButtonElement>) {
       event.preventDefault()
-      // event.currentTarget.disabled = true
       const letter = event.currentTarget.innerText
 
       setGuesses([...guesses, letter])
 
-      // if (randomWord.includes(letter.toLowerCase())) {
-      //   for (let i = 0; i < randomWord.length; i++) {
-      //     if (randomWord[i] == letter.toLowerCase()) {
-      //       setWord(word.insert(i, letter))
-      //     }
-      //   }
-      // }
-      // turn the box red
+      return (
+        <>
+          <h1>Do you want to build a snowman?</h1>
+          <main className="d-flex">
+            <img src="/snowman-images/step_7.png" height="300px"></img>
+            <ul>
+              {randomWord.split('').map((letter, i) => (
+                <li key={i}>{guesses.includes(letter) ? letter : '_'}</li>
+              ))}
+            </ul>
+            <AlphabetButtons />
+          </main>
+        </>
+      )
     }
 
-    //console.log(guessedWordArray)
-    // function transformClassName(letter: string): string | undefined {
-    //   if (letter) }
-
-    // do I need to make the ID a parameter of the object, assignKey?
+    // function compareClickWithRandomWord() {
+    //   //  Compare each button press to if that value is contained within .randomWord: string
+    //   //
+    //   //  If it is, identify where (and grab that letter's index from .randomWord)
+    //   //
+    //   //  Method to return the index number of where a button matches a letter within .randomWord
+    //   //
+    //   //  Display the letter (utilizing the corresponding index) on the game board
+    //
+    // if (randomWord.includes(letter.toLowerCase())) {
+    //   for (let i = 0; i < randomWord.length; i++) {
+    //     if (randomWord[i] == letter.toLowerCase()) {
+    //       setWord(word.insert(i, letter))
+    //     }
+    //   }
+    // }
+    // turn the box red
   }
 
-  return (
-    <>
-      <h1>Do you want to build a snowman?</h1>
-      <main className="d-flex">
-        <img src="/snowman-images/step_7.png" height="300px"></img>
-        <ul>
-          {randomWord.split('').map((letter, i) => (
-            <li key={i}>{guesses.includes(letter) ? letter : '_'}</li>
-          ))}
-        </ul>
-        <AlphabetButtons />
-      </main>
-    </>
-  )
-}
+  //console.log(guessedWordArray)
+  // function transformClassName(letter: string): string | undefined {
+  //   if (letter) }
 
-//  Compare each button press to if that value is contained within .randomWord: string
-//
-//  If it is, identify where (and grab that letter's index from .randomWord)
-//
-//  Method to return the index number of where a button matches a letter within .randomWord
-//
-//  Display the letter (utilizing the corresponding index) on the game board
+  // do I need to make the ID a parameter of the object, assignKey?
+}
